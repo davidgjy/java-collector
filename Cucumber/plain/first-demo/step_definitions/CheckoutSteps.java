cf93286b367027380ddac9da75d28b8be1d09b84
KG <davidgjy@163.com>
2018-05-08 16:09:18 +0800

    cucumber first demo

--- Cucumber/plain/first-demo/step_definitions/CheckoutSteps.java
+++ Cucumber/plain/first-demo/step_definitions/CheckoutSteps.java
@@ -10,23 +10,26 @@ package step_definitions;
 
 import cucumber.api.java.en.*;
 import cucumber.api.PendingException;
+import implementation.Checkout;
+import static org.junit.Assert.*;
 
 public class CheckoutSteps {
+    Checkout checkout;
+    int bananaPrice = 0;
+
     @Given("^the price of a \"(.*?)\" is (\\d+)c$")
-    public void thePriceOfAIsC(String arg1, int arg2) throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+    public void thePriceOfAIsC(String name, int price) throws Throwable {
+        bananaPrice = price;
     }
 
     @When("^I checkout (\\d+) \"(.*?)\"$")
-    public void iCheckout(int arg1, String arg2) throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+    public void iCheckout(int itemCount, String itemName) throws Throwable {
+        checkout = new Checkout();
+        checkout.add(itemCount, bananaPrice);
     }
 
     @Then("^the total price should be (\\d+)c$")
-    public void theTotalPriceShouldBeC(int arg1) throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+    public void theTotalPriceShouldBeC(int total) throws Throwable {
+        assertEquals(total, checkout.total());    
     }
 }
