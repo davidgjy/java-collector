bd25795a5cf2444ebe3f5f2dccf22ffc988e91cb
Kelvin Gu <jgu55@its.jnj.com>
2018-08-06 20:15:40 +0800

    mustache spring demo

---
+++ Template/mustache-spring/src/main/java/org/genesis/javacollect/mustache/mustachespring/MustacheSpringApplication.java
@@ -0,0 +1,12 @@
+package org.genesis.javacollect.mustache.mustachespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MustacheSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MustacheSpringApplication.class, args);
+	}
+}
