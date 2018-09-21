9b3101150fde7d77c5ad3c3e590837fbac24a43b
Kelvin Gu <jgu55@its.jnj.com>
2018-09-21 15:58:27 +0800

    spring security with mysql support

---
+++ SpringBoot/SpringSecurity/ss-thymeleaf-mysql/src/main/java/com/genesis/springboot/security/SpringSecurityApplication.java
@@ -0,0 +1,19 @@
+package com.genesis.springboot.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author KG(Kelvin Gu)
+ * @ClassName: SpringSecurityApplication
+ * @Package com.genesis.springboot.security
+ * @Description: spring boot entrance
+ * @date 2018/9/21 15:43
+ */
+@SpringBootApplication
+public class SpringSecurityApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityApplication.class, args);
+    }
+}
+
