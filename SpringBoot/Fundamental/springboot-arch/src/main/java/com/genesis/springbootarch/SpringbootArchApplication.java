59af743cab5740d57e87b05904bda11f96778526
Kelvin Gu <jgu55@its.jnj.com>
2019-04-22 17:14:29 +0800

    SpringBoot project init

---
+++ SpringBoot/Fundamental/springboot-arch/src/main/java/com/genesis/springbootarch/SpringbootArchApplication.java
@@ -0,0 +1,13 @@
+package com.genesis.springbootarch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootArchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootArchApplication.class, args);
+	}
+
+}
