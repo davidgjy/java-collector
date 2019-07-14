fe603836d78b78445f41bb60f37981187ee667be
KG <davidgjy@163.com>
2019-07-14 09:26:23 +0800

    spring-boot-funda

---
+++ SpringBoot/Mixtape/springboot-mix/springboot-funda/src/main/java/org/genesis/javacollector/springboot/funda/GsSbtFundaApplication.java
@@ -0,0 +1,16 @@
+package org.genesis.javacollector.springboot.funda;
+
+import org.springframework.boot.SpringApplication;
+
+/**
+ * @author: KG
+ * @description:
+ * @date: Created in 上午8:58 2019/7/14
+ * @modified by:
+ */
+
+public class GsSbtFundaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GsSbtFundaApplication.class, args);
+    }
+}
---
+++ SpringBoot/Mixtape/springboot-mix/springboot-funda/src/main/java/org/genesis/javacollector/springboot/funda/controller/InterceptorDemoController.java
@@ -0,0 +1,11 @@
+package org.genesis.javacollector.springboot.funda.controller;
+
+/**
+ * @author: KG
+ * @description:
+ * @date: Created in 上午9:00 2019/7/14
+ * @modified by:
+ */
+
+public class InterceptorDemoController {
+}
