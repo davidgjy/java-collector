aa4f13fc24a71679f3c2f25567f8ad68362bc323
Kelvin Gu <jgu55@its.jnj.com>
2019-11-23 09:18:49 +0800

    spring security sample ready

---
+++ SpringBoot/SpringSecurity/samples/default-mem-security/src/main/java/com/genesis/defaultmemsecurity/DefaultMemSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.genesis.defaultmemsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DefaultMemSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DefaultMemSecurityApplication.class, args);
+	}
+
+}
